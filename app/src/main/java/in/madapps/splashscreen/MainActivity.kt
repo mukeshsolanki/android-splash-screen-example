d687aef590255eb72e76d2f6c888e250569b528e
Mukesh Solanki <mukesh@madapps.in>
2019-03-20 11:53:42 -0400

    added theme

--- app/src/main/java/in/madapps/splashscreen/MainActivity.kt
+++ app/src/main/java/in/madapps/splashscreen/MainActivity.kt
@@ -1,12 +1,14 @@
 package `in`.madapps.splashscreen
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    //To mimic the black screen when data is being loaded in the activity
+    Thread.sleep(2000)
     setContentView(R.layout.activity_main)
   }
 }
